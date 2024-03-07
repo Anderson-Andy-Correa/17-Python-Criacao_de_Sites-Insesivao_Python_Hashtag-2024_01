8c8ae32dfcae251b0156bc58ab636ad127eff8a0
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-03-07 19:43:18 -0300

    init do projeto

---
+++ codigo.py
@@ -0,0 +1,13 @@
+# Título Hashzap
+# Botão de iniciar chat
+    # Clicou no botão
+        # Aparece um popup / modal
+            #Titulo: Bem vindo ao Hashzap
+            # campo: Escreva seu nome no chat
+            # Botão: Entrar no chat
+# Chat
+# Embaixo do chat
+    # campo: Digite sua mensagem
+    # Botão: enviar
+
+# Flat -> Framework do Python
