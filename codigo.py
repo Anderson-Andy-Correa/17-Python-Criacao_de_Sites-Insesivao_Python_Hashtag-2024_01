07428e8cec01116a477abea594d1cd2bf10a26a0
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-03-07 20:45:07 -0300

    Alterando texto de entrada ao chat

--- codigo.py
+++ codigo.py
@@ -19,22 +19,27 @@ def main(pagina): # Criar a funcção main
     texto = ft.Text("Hashzap")
 
     chat = ft.Column()
-    
+
     def enviar_mensagem(evento):
         print("Enviar mensagem")
 
 
-    mensagem = ft. TextField(label="Digite sua mensagem")
+    campo_mensagem = ft. TextField(label="Digite sua mensagem")
     botao_enviar = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
+    linha_enviar = ft.Row([campo_mensagem, botao_enviar])
 
     def entrar_chat(evento):
         popup.open=False # Fechar o popup
         pagina.remove(botao_iniciar) # Tirar o botão iniciar chat
         pagina.remove(texto) # Titar o titulo Hashzap
         pagina.add(chat) # Criar o Chat
-        pagina.add(mensagem) # colocar o campo de digitar mensagem
-        pagina.add(botao_enviar) # criar o botão de enviar
+        texto_entrada = ft.Text("Andy entrou no chat")
+        chat.controls.append(texto_entrada)
+         # colocar o campo de digitar mensagem
+         # criar o botão de enviar
+        pagina.add(linha_enviar)
         pagina.update() # Atualizar Página
+
 
     titulo_popup = ft.Text("Bem vindo ao Hashzap")
     nome_usuario = ft.TextField(label="Escreva o seu nome no chat")
@@ -54,7 +59,7 @@ def main(pagina): # Criar a funcção main
         pagina.update()
 
     botao_iniciar = ft.ElevatedButton("Iniciar Chat", on_click=abrir_popup)
-    
+
     pagina.add(texto)
     pagina.add(botao_iniciar)
 
